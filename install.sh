98cc6026d4c1c2558ea2e49a59a82ebc4c8fcc1e
Kacper Kuźniarski <kacper.kuzniarski@gmail.com>
2019-11-06 11:51:47 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && \
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim && \
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm && \
 bash ~/.tmux/plugins/tpm/scripts/install_plugins.sh && \
-ln -s -f ~/dotfiles/.vimrc ~/.vimrc && \
-ln -s -f ~/dotfiles/.tmux.conf ~/.tmux.conf && \
-ln -s -f ~/dotfiles/.bash_profile ~/.bash_profile && \
-ln -s -f ~/dotfiles/.gitconfig ~/.gitconfig && \
+ln -s -f ~/.dotfiles/.vimrc ~/.vimrc && \
+ln -s -f ~/.dotfiles/.tmux.conf ~/.tmux.conf && \
+ln -s -f ~/.dotfiles/.bash_profile ~/.bash_profile && \
+ln -s -f ~/.dotfiles/.gitconfig ~/.gitconfig && \
 vim +PluginInstall +qall
