d76b87144442a23e5294a266bd60a454eeb535d2
Kacper Kuźniarski <kacper.kuzniarski@gmail.com>
2019-11-08 10:53:27 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@ sudo apt-get update && \
 sudo apt-get install vim tmux git exuberant-ctags silversearcher-ag && \
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && \
 ~/.fzf/install && \
+wget https://github.com/sharkdp/fd/releases/download/v7.4.0/fd_7.4.0_amd64.deb && \
+sudo dpkg -i fd_7.4.0_amd64.deb && \
+rm fd_7.4.0_amd64.deb && \
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim && \
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm && \
 bash ~/.tmux/plugins/tpm/scripts/install_plugins.sh && \
