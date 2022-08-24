80ec23d1bf03da4f805453afca51428a3736e23f
kuzxnia <kacper.kuzniarski@gmail.com>
2022-08-24 15:10:19 +0200

    options refactor

--- .config/nvim/lua/user/options.lua
+++ .config/nvim/lua/user/options.lua
@@ -7,7 +7,6 @@ local options = {
   fileencoding = "utf-8",                  -- the encoding written to a file
   hlsearch = true,                         -- highlight all matches on previous search pattern
   ignorecase = true,                       -- ignore case in search patterns
-  -- mouse = "a",                             -- allow the mouse to be used in neovim
   pumheight = 10,                          -- pop up menu height
   showmode = false,                        -- we don't need to see things like -- INSERT -- anymore
   showtabline = 2,                         -- always show tabs
@@ -16,7 +15,6 @@ local options = {
   splitbelow = true,                       -- force all horizontal splits to go below current window
   splitright = true,                       -- force all vertical splits to go to the right of current window
   swapfile = false,                        -- creates a swapfile
-  -- termguicolors = true,                    -- set term gui colors (most terminals support this)
   timeoutlen = 1000,                        -- time to wait for a mapped sequence to complete (in milliseconds) 300 in my config
   undofile = true,                         -- enable persistent undo
   updatetime = 300,                        -- faster completion (4000ms default)
@@ -24,6 +22,7 @@ local options = {
   expandtab = true,                        -- convert tabs to spaces
   shiftwidth = 2,                          -- the number of spaces inserted for each indentation
   tabstop = 2,                             -- insert 2 spaces for a tab
+  softtabstop = 2,                         -- insert 2 spaces for a tab
   cursorline = true,                       -- highlight the current line
   number = true,                           -- set numbered lines
   relativenumber = false,                  -- set relative numbered lines
@@ -69,8 +68,14 @@ vim.g.mapleader = ' '
 vim.cmd[[set ruler nu nowrap laststatus=2 ]]
 vim.cmd[[set noswapfile autowrite foldlevelstart=99 ]]
 -- text format
+vim.cmd[[set cindent autoindent smarttab expandtab ]]
 vim.cmd[[setlocal conceallevel=3]]
-vim.cmd[[set tabstop=4 backspace=2 shiftwidth=4 cindent autoindent smarttab expandtab backspace=2 softtabstop=4 ]]
+-- indent(spacing) by filetype
+vim.cmd[[autocmd Filetype python setlocal ts=4 sw=4 expandtab]]
+vim.cmd[[autocmd Filetype javascript setlocal ts=4 sw=4 expandtab]]
+vim.cmd[[autocmd Filetype coffescript setlocal ts=4 sw=4 expandtab]]
+vim.cmd[[autocmd Filetype html setlocal ts=2 sw=2 expandtab]]
+vim.cmd[[autocmd Filetype dart setlocal ts=2 sw=2 expandtab]]
 -- searching
 vim.cmd[[set ignorecase smartcase incsearch hlsearch]]
 vim.cmd[[set wildignore+=*.o,*.obj,*.exe,*.so,*.dll,*.pyc,.svn,.hg,.bzr,.git,.sass-cache,*.class]]
