7c679cdb3d8398bc27b2dc3281701809f5f6cf2d
Kacper Kuźniarski <kacper.kuzniarski@allegro.com>
2023-04-22 20:48:44 +0200

    lvim update

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -7,9 +7,15 @@ lvim.format_on_save.enabled = false
 lvim.colorscheme = "onedark"
 
 -- keymappings [view all the defaults by pressing <leader>Lk]
-lvim.leader = "space"
+vim.leader = "space"
 -- add your own keymapping
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+-- tab navigation
+lvim.keys.normal_mode["Q"] = ":BufferKill<CR>"
+lvim.keys.normal_mode["L"] = ":BufferLineCycleNext<CR>"
+lvim.keys.normal_mode["H"] = ":BufferLineCyclePrev<CR>"
+-- fix coursor move to left after insert mode exit
+lvim.keys.insert_mode['<esc>'] = '<esc>`^'
 -- tmux navigation
 lvim.keys.normal_mode['<C-j>'] = '<Cmd>TmuxNavigateDown<CR>'
 lvim.keys.normal_mode['<C-h>'] = '<Cmd>TmuxNavigateLeft<CR>'
@@ -19,6 +25,12 @@ lvim.keys.normal_mode['<C-l>'] = '<Cmd>TmuxNavigateRight<CR>'
 lvim.builtin.which_key.mappings["c"] = {"<Plug>(comment_toggle_linewise_current)", "Comment toggle current line"}
 lvim.builtin.which_key.vmappings["c"] = {"<Plug>(comment_toggle_linewise_visual)", "Comment"}
 
+lvim.builtin.which_key.mappings["<space>"] = {
+  function()
+    require("lvim.core.telescope.custom-finders").find_project_files { }
+  end,
+  "Find File",
+}
 lvim.builtin.which_key.mappings["f"] = {
   function()
     require("lvim.core.telescope.custom-finders").find_project_files { }
@@ -35,45 +47,40 @@ lvim.builtin.which_key.mappings["f"] = {
 -- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
 -- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
 local _, actions = pcall(require, "telescope.actions")
-
 local picker_config = {
+  prompt_prefix = '🔍 ',
   previewer = nil,
   layout_strategy = 'vertical',
-  layout_config = { width = 0.9, height = 0.95 }
+  layout_config = { 
+    width = 0.95, 
+    height = 0.95, 
+    prompt_position = 'bottom',
+  }
 }
-
 lvim.builtin.telescope.pickers.git_files = picker_config
 lvim.builtin.telescope.pickers.find_files = picker_config
 lvim.builtin.telescope.pickers.live_grep = picker_config
-
-lvim.builtin.telescope.theme = 'dropdown'
+lvim.builtin.telescope.theme = 'default'
 lvim.builtin.telescope.defaults = {
-  prompt_prefix = '🔍 ',
-  layout_config = {
-    width = 0.9,
-    height = 0.95
+  layout_strategy = "horizontal",
+  borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+  mappings = {
+  -- for input mode
+    i = {
+      ["<esc>"] = actions.close,
+      ["<C-j>"] = actions.move_selection_next,
+      ["<C-k>"] = actions.move_selection_previous,
+      ["<C-n>"] = actions.cycle_history_next,
+      ["<C-p>"] = actions.cycle_history_prev,
+    },
+    -- for normal mode
+    n = {
+      ["<esc>"] = actions.close,
+      ["<C-j>"] = actions.move_selection_next,
+      ["<C-k>"] = actions.move_selection_previous,
+    },
   }
 }
-lvim.builtin.telescope.defaults.mappings = {
-  -- for input mode
-  i = {
-    ["<esc>"] = actions.close,
-    ["<C-j>"] = actions.move_selection_next,
-    ["<C-k>"] = actions.move_selection_previous,
-    ["<C-n>"] = actions.cycle_history_next,
-    ["<C-p>"] = actions.cycle_history_prev,
-  },
-  -- for normal mode
-  n = {
-    ["<esc>"] = actions.close,
-    ["<C-j>"] = actions.move_selection_next,
-    ["<C-k>"] = actions.move_selection_previous,
-  },
-}
-
--- Change theme settings
--- lvim.builtin.theme.options.dim_inactive = true
--- lvim.builtin.theme.options.style = "storm"
 
 -- Use which-key to add extra bindings with the leader-key prefix
 -- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
@@ -100,7 +107,6 @@ lvim.builtin.treesitter.ensure_installed = {
   "bash",
   "c",
   "javascript",
-  "json",
   "lua",
   "python",
   "typescript",
@@ -115,6 +121,21 @@ lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enable = true
 
 -- generic LSP settings
+lvim.lsp.installer.setup.automatic_servers_installation = false
+lvim.lsp.automatic_servers_installation = false
+vim.list_extend(
+  lvim.lsp.automatic_configuration.skipped_servers, 
+  { 
+    "lua_ls", 
+    "sumneko_lua",
+    "jedi_language_server",
+    "pyre",
+    "pylsp",
+    "pylyzer",
+    "ruff_lsp",
+  }
+)
+lvim.lsp.diagnostics.virtual_text = false
 
 -- -- make sure server will always be installed even if the server is in skipped_servers list
 -- lvim.lsp.installer.setup.ensure_installed = {
@@ -135,9 +156,23 @@ lvim.builtin.treesitter.highlight.enable = true
 
 -- ---configure a server manually. !!Requires `:LvimCacheReset` to take effect!!
 -- ---see the full default list `:lua print(vim.inspect(lvim.lsp.automatic_configuration.skipped_servers))`
--- vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright" })
--- local opts = {} -- check the lspconfig documentation for a list of all possible options
--- require("lvim.lsp.manager").setup("pyright", opts)
+vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright" })
+local opts = {
+  settings = {
+    python = {
+      pythonPath = vim.loop.cwd() .. '/.venv/bin/python',
+      venvPath = vim.loop.cwd() .. '/.venv/',
+      analysis = {
+        autoImportCompletion = true,
+        typeCheckingMode = "off",
+        autoSearchPaths = true,
+        diagnosticMode = "workspace",
+        useLibraryCodeForTypes = true,
+      },
+    },
+  },
+} -- check the lspconfig documentation for a list of all possible options
+require("lvim.lsp.manager").setup("pyright", opts)
 
 -- ---remove a server from the skipped list, e.g. eslint, or emmet_ls. !!Requires `:LvimCacheReset` to take effect!!
 -- ---`:LvimInfo` lists which server(s) are skipped for the current filetype
@@ -207,22 +242,46 @@ lvim.plugins = {
       require("auto-save").setup()
     end,
   },
-  -- {
-  --   "zbirenbaum/copilot-cmp",
-  --   event = "InsertEnter",
-  --   dependencies = { "zbirenbaum/copilot.lua" },
-  --   config = function()
-  --     vim.defer_fn(function()
-  --       require("copilot").setup() -- https://github.com/zbirenbaum/copilot.lua/blob/master/README.md#setup-and-configuration
-  --       require("copilot_cmp").setup() -- https://github.com/zbirenbaum/copilot-cmp/blob/master/README.md#configuration
-  --     end, 100)
-  --   end,
-  -- },
+  {
+    "zbirenbaum/copilot.lua",
+    event = { "VimEnter" },
+    config = function()
+      vim.defer_fn(function()
+        require("copilot").setup {
+          plugin_manager_path = os.getenv "LUNARVIM_RUNTIME_DIR" .. "/site/pack/packer",
+        }
+      end, 100)
+    end,
+  },
+  {
+    "zbirenbaum/copilot-cmp",
+    after = { "copilot.lua" },
+    config = function()
+      require("copilot_cmp").setup()
+    end,
+  },
   {
     "ray-x/lsp_signature.nvim",
     event = "BufRead",
     config = function() require"lsp_signature".on_attach() end,
   },
+  {
+   "ggandor/lightspeed.nvim",
+    event = "BufRead",
+  },
+  {
+    "ethanholz/nvim-lastplace",
+    event = "BufRead",
+    config = function()
+      require("nvim-lastplace").setup({
+        lastplace_ignore_buftype = { "quickfix", "nofile", "help" },
+        lastplace_ignore_filetype = {
+          "gitcommit", "gitrebase", "svn", "hgcommit",
+        },
+        lastplace_open_folds = true,
+     })
+    end,
+   },
 }
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
